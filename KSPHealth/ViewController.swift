8ea838bd6b03ad729578c2a318caeee4e5f2ac41
Nguyen The Phuong <phuong.nt2417@gnt.com.vn>
2018-10-30 10:08:34 +0700

    Initial Commit

---
+++ KSPHealth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KSPHealth
+//
+//  Created by Nguyen The Phuong on 10/30/18.
+//  Copyright © 2018 Nguyen The Phuong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
